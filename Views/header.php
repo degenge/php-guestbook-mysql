45744b95d9c55b380d75c84e32c20e27e8e1a49d
Gerrit Degenhardt <gerrit@degenhardt.be>
2020-09-30 16:41:55 +0200

    Working version

---
+++ Views/header.php
@@ -0,0 +1,11 @@
+<header class="bg-gray-200" >
+
+    <div class="container mx-auto px-6 py-3" >
+
+        <div class="flex items-center justify-between" >
+            <div class="w-full text-green-700 md:text-center text-5xl font-semibold" >Guestbook DB</div >
+        </div >
+
+    </div >
+
+</header >
